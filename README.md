17a21d942d7a8a646296191bac4c236ba2530c51
Vectorized <webby1111@hotmail.com>
2024-08-03 18:22:04 +0000

    T

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 ## Solady Foundry Starter
 
-Start a Foundry project by running this in your terminal:
+### Create a Foundry Project
+
+Create a Foundry project by running this in your terminal, in an empty directory:
 
 ```shell
 curl -s https://raw.githubusercontent.com/Vectorized/foundry-starter/main/create-foundry-project.sh | bash
 ```
 
-## Important
+### Install Solady Test Framework
 
-This is NOT a template. 
+If your Foundry project has already been created, to install the Solady Test Framework:
+
+```shell
+curl -s https://raw.githubusercontent.com/Vectorized/foundry-starter/main/install-solady-test.sh | bash
+```
+
+### Important
+
+This is NOT a template.  
 
 This is simply a script to initialize a Foundry project with Solady recommended settings.
--- install-solady-test.sh
+++ install-solady-test.sh
@@ -1,18 +1,30 @@
-mkdir -p test;
-mkdir -p test/utils;
+downloadTest() {
+  mkdir -p "$1";
+  mkdir -p "$1/utils";
 
-curl -o test/utils/TestPlus.sol https://raw.githubusercontent.com/Vectorized/solady/main/test/utils/TestPlus.sol;
-curl -o test/utils/Brutalizer.sol https://raw.githubusercontent.com/Vectorized/solady/main/test/utils/Brutalizer.sol;
+  curl -o "$1/utils/TestPlus.sol" https://raw.githubusercontent.com/Vectorized/solady/main/test/utils/TestPlus.sol;
+  curl -o "$1/utils/Brutalizer.sol" https://raw.githubusercontent.com/Vectorized/solady/main/test/utils/Brutalizer.sol;
 
-echo "// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+  echo "// SPDX-License-Identifier: MIT
+  pragma solidity ^0.8.4;
 
-import \"forge-std/Test.sol\";
-import \"./TestPlus.sol\";
+  import \"forge-std/Test.sol\";
+  import \"./TestPlus.sol\";
 
-contract SoladyTest is Test, TestPlus {
-    /// @dev Alias for \`_hem\`.
-    function _bound(uint256 x, uint256 min, uint256 max) internal pure virtual override returns (uint256) {
-        return _hem(x, min, max);
-    }
-}" > test/utils/SoladyTest.sol;
+  contract SoladyTest is Test, TestPlus {
+      /// @dev Alias for \`_hem\`.
+      function _bound(uint256 x, uint256 min, uint256 max) internal pure virtual override returns (uint256) {
+          return _hem(x, min, max);
+      }
+  }" > "$1/utils/SoladyTest.sol";
+
+  forge fmt;
+}
+
+if [ -d "test" ]; then
+  downloadTest("test")
+elif [ -d "tests" ]; then
+  downloadTest("tests")
+else
+  echo "No test directory found"
+fi
