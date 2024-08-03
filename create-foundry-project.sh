84c3cdbdf776cefe25398cfa35a623e3c048615f
Vectorized <webby1111@hotmail.com>
2024-08-03 17:22:53 +0000

    chore: forge init

--- create-foundry-project.sh
+++ create-foundry-project.sh
@@ -1,4 +1,55 @@
+forge init --force;
 
+forge install vectorized/solady;
+
+mkdir test/utils;
+cp lib/solady/test/utils/TestPlus.sol test/utils/TestPlus.sol;
+cp lib/solady/test/utils/Brutalizer.sol test/utils/Brutalizer.sol;
+
+echo "// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import \"forge-std/Test.sol\";
+import \"./TestPlus.sol\";
+
+contract SoladyTest is Test, TestPlus {
+    /// @dev Alias for \`_hem\`.
+    function _bound(uint256 x, uint256 min, uint256 max)
+        internal
+        pure
+        virtual
+        override
+        returns (uint256)
+    {
+        return _hem(x, min, max);
+    }
+}" > test/utils/SoladyTest.sol;
+
+echo "// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import \"./SoladyTest.sol\";
+import {Counter} from \"../src/Counter.sol\";
+
+contract CounterTest is SoladyTest {
+    Counter public counter;
+
+    function setUp() public {
+        counter = new Counter();
+        counter.setNumber(0);
+    }
+
+    function testIncrement() public {
+        counter.increment();
+        assertEq(counter.number(), 1);
+    }
+
+    function testSetNumber(uint256 x) public {
+        counter.setNumber(x);
+        assertEq(counter.number(), x);
+    }
+}
+" > test/Counter.t.sol;
 
 echo "# Foundry Configuration File
 # Default definitions: https://github.com/gakonst/foundry/blob/b7917fa8491aedda4dd6db53fbb206ea233cd531/config/src/lib.rs#L782
@@ -22,3 +73,43 @@ runs = 256
 depth = 15
 runs = 10
 " > foundry.toml;
+
+echo "
+# NodeJS files
+node_modules/
+coverage
+coverage.json
+typechain
+
+# Hardhat files
+cache/
+artifacts/
+cache/
+out/
+
+# Ignore Environment Variables!
+.env
+.env.prod
+
+# Ignore all vscode settings
+.vscode/
+
+# Ignore all jetbrain settings
+.idea/
+
+# Ignore flattened files
+flattened.txt
+broadcast
+# Coverage
+lcov.info
+
+# Woke testing
+.wake
+pytypes
+__pycache__/
+*.py[cod]
+.hypothesis/
+wake-coverage.cov
+" > .gitignore;
+
+forge fmt;
